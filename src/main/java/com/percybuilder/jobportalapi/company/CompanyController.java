655c516cfd6fab6df4eb98070d4886b6b5dd4535
Percy Osunde <posunde@my.centennialcollege.ca>
2026-06-06 14:42:17 -0400

    Implement company listing API

--- src/main/java/com/percybuilder/jobportalapi/company/CompanyController.java
+++ src/main/java/com/percybuilder/jobportalapi/company/CompanyController.java
@@ -1,20 +1,18 @@
 package com.percybuilder.jobportalapi.company;
 
 import com.percybuilder.jobportalapi.company.dto.CompanyResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+@RequiredArgsConstructor
 
 @RestController
 public class CompanyController {
 
     private final CompanyService companyService;
-
-    public CompanyController(CompanyService companyService) {
-        this.companyService = companyService;
-    }
 
     @GetMapping("/api/v1/companies")
     public ResponseEntity<List<CompanyResponse>> getAllCompanies() {
