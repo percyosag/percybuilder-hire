2071ea902e61186c31c388982124dbf599e2044b
Percy Osunde <posunde@my.centennialcollege.ca>
2026-06-15 17:58:12 -0400

    Enhance home page with live data

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { jobsApi } from "../api/jobsApi";
 import { companiesApi } from "../api/companiesApi";
+import { contactsApi } from "../api/contactsApi";
 
 export const store = configureStore({
   reducer: {
     [jobsApi.reducerPath]: jobsApi.reducer,
     [companiesApi.reducerPath]: companiesApi.reducer,
+    [contactsApi.reducerPath]: contactsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(jobsApi.middleware)
-      .concat(companiesApi.middleware),
+      .concat(companiesApi.middleware)
+      .concat(contactsApi.middleware),
 });
